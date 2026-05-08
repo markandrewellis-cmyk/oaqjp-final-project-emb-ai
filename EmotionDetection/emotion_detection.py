46cd053bdf76cbe18ac7fd30cd13cf2cde19a75f
markandrewellis-cmyk <markandrewellis@gmail.com>
2026-05-08 09:31:51 -0400

    Initial Commit

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,22 @@
+import requests
+
+def emotion_detection(text_to_analyse): 
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = {"raw_document":{"text": text_to_analyse}}
+    header = {"grpc-metadata-mm-model-id":"emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json = myobj, headers = header)
+    
+    response_dict = response.json()
+    emotions = response_dict['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': dominant_emotion
+    }
+response=emotion_detection("I am so happy doing this")
+print(response)
